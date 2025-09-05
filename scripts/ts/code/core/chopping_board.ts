628a69435aefe4c3f953661bc5589cab9bb9ed9c
逍遙 <lll090407@gmail.com>
2025-09-06 00:39:57 +0800

    添加了砧板的生野牛肉和烤野牛肉。修正生野牛肉、烤野牛肉、生野牛肉片、烤野牛肉片在物品列表的分類

--- scripts/ts/code/core/chopping_board.ts
+++ scripts/ts/code/core/chopping_board.ts
@@ -1,13 +1,17 @@
 import { Block, BlockPermutation, BlockStates, BlockStateType, ItemStack, system, world } from "@minecraft/server";
 import { UFLib } from "../lib/uflib/uflib_core.js";
 
+/**
+ * 砧板系統
+ */
 
 world.beforeEvents.playerInteractWithBlock.subscribe(signal => {
     if(signal.isFirstEvent){
         if (signal.block.typeId == "miki:chopping_board"){
-            // 砧板
+            // 取得方塊狀態與物品
             if (signal.itemStack === undefined) return;
             const allStates = signal.block.permutation.getAllStates();
+            // 依物品判斷執行動作
             switch (signal.itemStack.typeId) {
                 case "minecraft:pufferfish":
                     // 河豚
@@ -20,13 +24,53 @@ world.beforeEvents.playerInteractWithBlock.subscribe(signal => {
                     }
                     break;
 
+                case "miki:raw_bison_meat":
+                    // 生野牛肉
+                    if (allStates["chopping_board:item"] == 0) {
+                        allStates["chopping_board:item"] = 2;
+                        system.run(() => {
+                            signal.block.setPermutation(BlockPermutation.resolve("miki:chopping_board", allStates));
+                            signal.player.runCommand("clear @s miki:raw_bison_meat -1 1");
+                        });
+                    }
+                    break;
+
+                case "miki:cooked_bison_meat":
+                    // 烤野牛肉
+                    if (allStates["chopping_board:item"] == 0) {
+                        allStates["chopping_board:item"] = 3;
+                        system.run(() => {
+                            signal.block.setPermutation(BlockPermutation.resolve("miki:chopping_board", allStates));
+                            signal.player.runCommand("clear @s miki:cooked_bison_meat -1 1");
+                        });
+                    }
+                    break;
+
                 case "miki:kitchen_knife":
                     // 菜刀
                     let used: boolean = false;
+                    // 依砧板狀態切換行為
                     switch (allStates["chopping_board:item"]) {
                         case 1:
+                            // 有河豚
                             system.run(() => {
                                 world.getDimension(signal.player.dimension.id).spawnItem(new ItemStack("miki:clean_pufferfish"), signal.block.location);
+                            });
+                            used = true;
+                            break;
+
+                        case 2:
+                            // 有生野牛肉
+                            system.run(() => {
+                                world.getDimension(signal.player.dimension.id).spawnItem(new ItemStack("miki:raw_bison_slice", 2), signal.block.location);
+                            });
+                            used = true;
+                            break;
+
+                        case 3:
+                            // 有烤野牛肉
+                            system.run(() => {
+                                world.getDimension(signal.player.dimension.id).spawnItem(new ItemStack("miki:cooked_bison_slice", 3), signal.block.location);
                             });
                             used = true;
                             break;
@@ -34,6 +78,7 @@ world.beforeEvents.playerInteractWithBlock.subscribe(signal => {
                         default:
                             break;
                     }
+                    // 有使用的話，扣耐久，恢復砧板狀態
                     if(used){
                         allStates["chopping_board:item"] = 0;
                         system.run(() => {
